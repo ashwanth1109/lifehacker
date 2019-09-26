f88f39b35ab373e336878c786f83644943593f35
Ashwanth A R <ashwanth1109@gmail.com>
2019-09-27 01:47:37 +0530

    Clean up rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -24,9 +24,7 @@ export default {
       plugins: [
         // 'external-helpers',
         "@babel/plugin-proposal-class-properties",
-        "@babel/plugin-proposal-object-rest-spread",
-        "@babel/plugin-transform-runtime",
-        "@babel/plugin-proposal-object-rest-spread"
+        "@babel/plugin-transform-runtime"
       ]
     }),
     commonjs(),
